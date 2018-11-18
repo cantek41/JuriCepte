43272a69d3810da0e7bd4d9b4becc5aed90d772d
Furkankyl <furkan.kayali@bil.omu.edu.tr>
2018-11-18 10:14:14 +0300

    user methods added
    user sign in an sign up methods added

--- app/src/main/java/com/juricepte/can/juricepte/FirebaseMethods.java
+++ app/src/main/java/com/juricepte/can/juricepte/FirebaseMethods.java
@@ -10,6 +10,10 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -25,15 +29,36 @@ import static android.content.ContentValues.TAG;
 
     public class FirebaseMethods {
         FirebaseFirestore db;
+        FirebaseAuth auth;
         private String eventCollectionName = "Events";
         private String groupCollectionName = "Groups";
         private String ratingCollectionName = "Ratings";
 
         public FirebaseMethods() {
             this.db = FirebaseFirestore.getInstance();
+            this.auth = FirebaseAuth.getInstance();
 
         }
+        public void signIn(String email,String password){
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText("signIn", ""+authResult.getUser().getDisplayName(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        public void signUp(String email, String password, final String name){
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    FirebaseUser user = authResult.getUser();
 
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                            .setDisplayName(name).build();
+                    user.updateProfile(profileUpdates);
+                }
+            });
+        }
         public List<Action> getAllEvents() {
             final List<Action>  eventList = new ArrayList<>();
             db.collection(eventCollectionName).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
